40c0c45c9509f93af8a907503b85f7a472cc57ca
lai2-01 <lai2-01@itismeucci.com>
2021-10-13 13:34:35 +0200

    Commit

--- src/main/java/com/itismeucci/bencini/MultiServer.java
+++ src/main/java/com/itismeucci/bencini/MultiServer.java
@@ -1,23 +1,54 @@
 package com.itismeucci.bencini;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MultiServer {
+
+    List<ServerThread> threadList = new ArrayList<>();
+    ServerSocket serverSocket = null;
+
     public void start(){
         try {
-            ServerSocket serverSocket = new ServerSocket(6789);
+            serverSocket = new ServerSocket(6789);
             for(;;){
                 System.out.println("1 Server in attesa...");
                 Socket socket = serverSocket.accept();
                 System.out.println("3 Server socket " + socket);
-                ServerThread serverThread = new ServerThread(socket);
+                ServerThread serverThread = new ServerThread(socket, this);
                 serverThread.start();
+
+                threadList.add(serverThread);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Errore durante l'instanza del server");
             System.exit(1);
         }
+    }
+
+    public void stop() {
+
+        // per ogni elemento della lista chiudere il socket
+        for(int i = 0; i < threadList.size(); i++){
+            try {
+                threadList.get(i).close();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+        }
+
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 }
